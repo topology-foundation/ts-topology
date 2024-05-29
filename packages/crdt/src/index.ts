3b93b900aa91cd79783972f891b6114533255e1f
droak <me+git@droak.sh>
2024-05-29 14:35:59 +0200

    index gset and 2pset

--- packages/crdt/src/index.ts
+++ packages/crdt/src/index.ts
@@ -1,3 +1,5 @@
 // TODO dumb export logic, fix this
 export { GCounter } from "./builtins/GCounter";
 export { PNCounter } from "./builtins/PNCounter";
+export { GSet } from "./builtins/GSet";
+export { TwoPSet } from "./builtins/2PSet";
