968277660597ee1de2cda3f29d3de70e34663aaf
droak <me+git@droak.sh>
2024-06-02 04:48:44 +0200

    add logging

--- packages/node/src/run_relay.ts
+++ packages/node/src/run_relay.ts
@@ -2,7 +2,10 @@ import { createRelayNode } from "@topologygg/network";
 
 export async function start() {
   const node = await createRelayNode();
-  console.log(node.peerId.toString());
+  console.log("peer_id:", node.peerId.toString());
+  for (let ma of node.getMultiaddrs()) {
+    console.log(ma);
+  }
 }
 
 start();
