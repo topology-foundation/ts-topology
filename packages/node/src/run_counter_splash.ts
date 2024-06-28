a7a8eee0d629f3593cc2ac8f0a33ff981590e7e1
droak <me+git@droak.sh>
2024-06-29 06:26:20 +0900

    logger to debug size

--- packages/node/src/run_counter_splash.ts
+++ packages/node/src/run_counter_splash.ts
@@ -1,12 +1,20 @@
 import { TopologyNode } from ".";
 import { Canvas } from "./objects/canvas";
+import * as fs from "fs";
 
 export async function start() {
   const node = new TopologyNode();
   await node.start();
 
-  let canvas = new Canvas(node.getPeerId(), 1000, 1000);
+  let canvas = new Canvas(node.getPeerId(), 3000, 4000);
   canvas.id = "topology::counter_splash";
+  fs.writeFile("./object.json", JSON.stringify(canvas), (err: any) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("File has been created");
+  });
   node.createObject(canvas);
 }
 
