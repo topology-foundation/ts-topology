a8c7e6779a94148bb3cbc770f0b2eff28a3dd04d
droak <me+git@droak.sh>
2024-07-07 23:24:47 +0400

    modify canvas

--- packages/node/src/objects/canvas.ts
+++ packages/node/src/objects/canvas.ts
@@ -1,73 +1,56 @@
+import { GSet } from "@topology-foundation/crdt";
 import { TopologyObject } from "@topology-foundation/object";
-import { IPixel, Pixel } from "./pixel";
 
 export interface ICanvas extends TopologyObject {
   width: number;
   height: number;
-  canvas: IPixel[][];
-  splash(
-    node_id: string,
+  sprays: GSet<String>;
+  addSpray(
+    timestamp: number,
     offset: [number, number],
-    size: [number, number],
-    rgb: [number, number, number],
+    sprayType: number,
   ): void;
-  paint(
-    nodeId: string,
-    offset: [number, number],
-    rgb: [number, number, number],
-  ): void;
-  pixel(x: number, y: number): IPixel;
+  getSprays(): GSet<String>;
   merge(peerCanvas: Canvas): void;
 }
 
 export class Canvas extends TopologyObject implements ICanvas {
   width: number;
   height: number;
-  canvas: IPixel[][];
+  sprays: GSet<String>;
 
-  constructor(peerId: string, width: number, height: number) {
+  constructor(
+    peerId: string,
+    width: number,
+    height: number,
+    sprays?: Set<String>,
+  ) {
     super(peerId);
     this.width = width;
     this.height = height;
-    this.canvas = Array.from(new Array(width), () =>
-      Array.from(new Array(height), () => new Pixel(peerId)),
-    );
+    this.sprays = new GSet<String>(sprays ? sprays : new Set<String>());
   }
 
-  splash(
-    node_id: string,
+  addSpray(
+    timestamp: number,
     offset: [number, number],
-    size: [number, number],
-    rgb: [number, number, number],
+    sprayType: number,
   ): void {
-    if (offset[0] < 0 || this.width < offset[0]) return;
-    if (offset[1] < 0 || this.height < offset[1]) return;
-
-    for (let x = offset[0]; x < this.width || x < offset[0] + size[0]; x++) {
-      for (let y = offset[1]; y < this.height || y < offset[1] + size[1]; y++) {
-        this.canvas[x][y].paint(node_id, rgb);
-      }
-    }
+    if (
+      offset[0] < 0 ||
+      offset[0] >= this.width ||
+      offset[1] < 0 ||
+      offset[1] >= this.height
+    )
+      return;
+    this.sprays.add(`[${timestamp},[${offset[0]},${offset[1]}],${sprayType}]`);
   }
 
-  paint(
-    nodeId: string,
-    offset: [number, number],
-    rgb: [number, number, number],
-  ): void {
-    if (offset[0] < 0 || this.canvas.length < offset[0]) return;
-    if (offset[1] < 0 || this.canvas[offset[0]].length < offset[1]) return;
-
-    this.canvas[offset[0]][offset[1]].paint(nodeId, rgb);
-  }
-
-  pixel(x: number, y: number): IPixel {
-    return this.canvas[x][y];
+  getSprays(): GSet<String> {
+    return this.sprays;
   }
 
   merge(peerCanvas: Canvas): void {
-    this.canvas.forEach((row, x) =>
-      row.forEach((pixel, y) => pixel.merge(peerCanvas.pixel(x, y))),
-    );
+    this.sprays.merge(peerCanvas.getSprays());
   }
 }
