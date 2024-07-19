24b1eac8bfa53f65d38222f4e479322375bf3c30
joao <jrbotica10@gmail.com>
2024-07-19 17:55:22 +0100

    feat: LWW-element-set added, needs testing

---
+++ packages/crdt/src/builtins/LWWElementSet/index.ts
@@ -0,0 +1,82 @@
+import { GSet } from "../GSet/index.js";
+
+interface Element {
+    bias: Bias,
+    timestamp: number
+}
+
+enum Bias {
+    ADD,
+    REMOVE
+}
+
+export class LWWElementSet<T> {
+    private _adds: Map<T, Element>;
+    private _removes: Map<T, Element>;
+
+    constructor(adds: Map<T, Element>, removes: Map<T, Element>) {
+        this._adds = adds;
+        this._removes = removes;
+    }
+    // check for timestamps
+    lookup(element: T): boolean {
+
+        const addElemInfo = this._adds.get(element);
+        const remElemInfo = this._removes.get(element);
+
+        if (addElemInfo !== undefined &&
+            (remElemInfo === undefined || addElemInfo.timestamp > remElemInfo.timestamp)) {
+            return true;
+        }
+        return false;
+    }
+
+    add(element: T, timestamp: number, bias: Bias): void {
+        this._adds.set(element, { timestamp, bias });
+    }
+
+    remove(element: T, timestamp: number, bias: Bias): void {
+        this._adds.set(element, { timestamp, bias });
+    }
+
+    adds(): Map<T, Element> {
+        return this._adds;
+    }
+
+    removes(): Map<T, Element> {
+        return this._removes;
+    }
+
+    compare(peerSet: LWWElementSet<T>): boolean {
+        return (
+            this._adds.entries() == peerSet._adds.entries() &&
+            this._removes.entries() == peerSet._removes.entries()
+        );
+    }
+
+    merge(peerSet: LWWElementSet<T>): void {
+
+        for (let e of peerSet._adds.entries()) {
+            if (!this._adds.has(e[0]) || this.compareTimestamp(this._adds.get(e[0])!, e[1])) {
+                this._adds.set(e[0], e[1]);
+            }
+        }
+
+        for (let e of peerSet._removes.entries()) {
+            if (!this._removes.has(e[0]) || this.compareTimestamp(this._removes.get(e[0])!, e[1])) {
+                this._removes.set(e[0], e[1]);
+            }
+        }
+
+    }
+
+    compareTimestamp(thisElem: Element, other: Element) {
+
+        const timestampComparison = thisElem.timestamp - other.timestamp;
+
+        if (timestampComparison === 0) {
+            return thisElem.bias !== Bias.ADD;
+        }
+        return timestampComparison < 0;
+    }
+}
