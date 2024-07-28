222ffa48bd8f807da17933ff834d126d4ca9bc0a
joao <jrbotica10@gmail.com>
2024-07-28 23:44:26 +0100

    fix: added missing files

---
+++ packages/crdt/src/builtins/GCounter/index.ts
@@ -0,0 +1,41 @@
+/// GCounter with support for state and op changes
+export class GCounter {
+  globalCounter: number;
+  // instead of standard incremental id for replicas
+  // we map the counter with the node id
+  counts: { [nodeId: string]: number };
+
+  constructor(counts: { [nodeId: string]: number }) {
+    this.globalCounter = Object.values(counts).reduce((a, b) => a + b, 0);
+    this.counts = counts;
+  }
+
+  value(): number {
+    return this.globalCounter;
+  }
+
+  increment(nodeId: string, amount: number): void {
+    this.globalCounter += amount;
+    this.counts[nodeId] += amount;
+  }
+
+  compare(peerCounter: GCounter): boolean {
+    for (let key in Object.keys(this.counts)) {
+      if (this.counts[key] > peerCounter.counts[key]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  merge(peerCounter: GCounter): void {
+    let temp: { [nodeKey: string]: number } = Object.assign(
+      {},
+      this.counts,
+      peerCounter.counts,
+    );
+    Object.keys(temp).forEach((key) => {
+      this.counts[key] = Math.max(this.counts[key], peerCounter.counts[key]);
+    });
+  }
+}
