9d1eafc345d0d01a0e47c90b86b30633a8d78f9d
0xmugen <pierre.semanne@gmail.com>
2024-07-31 20:10:35 +0200

    front end tests

--- examples/wasm/src/index.ts
+++ examples/wasm/src/index.ts
@@ -1,15 +1,13 @@
 import { loadWasmModule } from "./wasmLoader";
 
 async function main() {
-  const canvasWasm = await loadWasmModule("../build/canvas.wasm");
-  const pixelWasm = await loadWasmModule("../build/pixel.wasm");
+  const canvasWasm = await loadWasmModule("/build/canvas.wasm");
+  const pixelWasm = await loadWasmModule("/build/pixel.wasm");
 
-  const width = 5;
-  const height = 10;
-  const canvas = new canvasWasm.Canvas(width, height);
-  canvas.splash("peerId", [0, 0], [2, 2], [255, 0, 0]);
+  (window as any).canvasWasm = canvasWasm;
+  (window as any).pixelWasm = pixelWasm;
 
-  console.log(canvas);
+  console.log("WASM Modules Loaded", { canvasWasm, pixelWasm });
 }
 
 main();
--- examples/wasm/src/wasmLoader.ts
+++ examples/wasm/src/wasmLoader.ts
@@ -1,7 +1,9 @@
 export async function loadWasmModule(path: string): Promise<any> {
-    const response = await fetch(path);
-    const buffer = await response.arrayBuffer();
-    const module = await WebAssembly.instantiate(buffer);
-    return module.instance.exports;
+  const response = await fetch(path);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch ${path}`);
   }
-  
+  const buffer = await response.arrayBuffer();
+  const module = await WebAssembly.instantiate(buffer, {});
+  return module.instance.exports;
+}
