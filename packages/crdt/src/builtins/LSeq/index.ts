5725447a097f14878db56f585ce8551deb8fca43
joao <jrbotica10@gmail.com>
2024-08-06 18:07:03 +0100

    fix: merge fixed and tests done, missing readme

--- packages/crdt/src/builtins/LSeq/index.ts
+++ packages/crdt/src/builtins/LSeq/index.ts
@@ -1,54 +1,82 @@
 type VPointer = {
-    sequence: Uint8Array,
+    sequence: number[],
     id: string
 };
 
 type Vertex<T> = {
     vPointer: VPointer,
     element: T
-}
+};
 
 export class LSeq<T> {
+    private _vertices: Vertex<T>[];
+    private _nodeId: string;
 
-    private _vertices: Array<Vertex<T>>;
-
-    constructor() {
-        this._vertices = new Array<Vertex<T>>();
+    constructor(nodeId: string) {
+        this._nodeId = nodeId;
+        this._vertices = [];
     }
 
-    insert(replicaId: string, index: number, content: T): void {
-        const left = index == 0 ? new Uint8Array() : this._vertices[index - 1].vPointer.sequence;
-        const right = index == this._vertices.length ? new Uint8Array() : this._vertices[index].vPointer.sequence;
-        const pointer = {sequence: generateSeq(left, right), id: replicaId};
-        this._vertices.splice(index, 0, {vPointer: pointer, element: content});
+    getVertices(): Vertex<T>[] {
+        return this._vertices;
+    }
+
+    getNodeId(): string {
+        return this._nodeId;
+    }
+
+    insert(index: number, content: T): void {
+        const left = index === 0 ? [] : this._vertices[index - 1].vPointer.sequence;
+        const right = index === this._vertices.length ? [] : this._vertices[index].vPointer.sequence;
+        const pointer = { sequence: generateSeq(left, right), id: this._nodeId };
+        const idx = this._vertices.findIndex( vertex => compareSeq(vertex.vPointer.sequence, pointer.sequence) >= 0);
+        const newVertices = this._vertices;
+        newVertices.splice(idx >= 0 ? idx  : this._vertices.length, 0, { vPointer: pointer, element: content });
+        this._vertices = newVertices;
     }
 
     delete(index: number): void {
-        if(index >= 0 && index < this._vertices.length) {
-            this._vertices.splice(index, 1);
+        const newVertices = this._vertices;
+        if (index >= 0 && index < newVertices.length) {
+            newVertices.splice(index, 1);
         }
+        this._vertices = newVertices;
     }
 
     query(): T[] {
-        return this._vertices.map(( {element}) => element);
+        return this._vertices.map(({ element }) => element);
+    }
+
+    merge(otherLSeq: LSeq<T>): void {
+        const newVertices = this._vertices;
+        otherLSeq.getVertices().forEach((value) => {
+            if(!newVertices.some( vertex => vertex.vPointer === value.vPointer)) {
+                newVertices.splice(otherLSeq._vertices.indexOf(value),0, value);
+            }
+        });
+        this._vertices = newVertices;
     }
 }
 
-// Compare virtual pointers
-function compareVPointers(vptr1: VPointer, vptr2: VPointer): number {
-    return 0;
+function compareSeq(seq1: number[], seq2: number[]): number {
+    const len = Math.min(seq1.length, seq2.length);
+    for (let i = 0; i < len; i++) {
+        if (seq1[i] !== seq2[i]) {
+            return seq1[i] - seq2[i];
+        }
+    }
+    return seq1.length - seq2.length;
 }
 
-// Generates sequence
-function generateSeq(lo: Uint8Array, hi: Uint8Array): Uint8Array {
+function generateSeq(lo: number[], hi: number[]): number[] {
     let array: number[] = [];
     let i = 0;
-    while(true) {
-        let min = i >= lo.length ? 0 : lo[i];
-        let max = i >= hi.length ? 255 : hi[i];
-        if(min + 1 < max) {
+    while (true) {
+        const min = i >= lo.length ? 0 : lo[i];
+        const max = i >= hi.length ? 255 : hi[i];
+        if (min + 1 < max) {
             array.push(min + 1);
-            return new Uint8Array(array);
+            return array;
         } else {
             array.push(min);
             i++;
--- packages/crdt/tests/LSeq.test.ts
+++ packages/crdt/tests/LSeq.test.ts
@@ -4,12 +4,70 @@ import { LSeq } from "../src/builtins/LSeq";
 describe("LSeq Tests", () => {
 
     test("Test Insert", () => {
-        let lseq = new LSeq();
-        lseq.insert("node1", 0, 'a');
-        lseq.insert("node1", 1, 'b');
-        lseq.insert("node1", 2, 'c');
-        expect(lseq.query()).toBe("abc");
+        let lseq = new LSeq("node1");
+
+        lseq.insert(0,'hi');
+        lseq.insert(1,'*');
+        lseq.insert(2,'!');
+    
+        expect(lseq.query().join('')).toBe("hi*!");
+
+        lseq.insert(2,'mom');
+        lseq.insert(2,'zom');
+        lseq.insert(2,'dad');
+        
+        expect(lseq.query().join('')).toBe("hi*dadzommom!");
     });
 
-   
+    test("Test Remove", () => {
+        let lseq = new LSeq("node1");
+        
+        lseq.insert(0,'hi');
+        lseq.insert(1,'*');
+        lseq.insert(2,'mom');
+        lseq.insert(3,'!');
+    
+        expect(lseq.query().join('')).toBe("hi*mom!");
+
+        lseq.delete(2);
+
+        expect(lseq.query().join('')).toBe("hi*!");
+
+        lseq.insert(2,'mom');
+        lseq.insert(2,'dad');
+
+        expect(lseq.query().join('')).toBe("hi*dadmom!");
+
+        lseq.delete(2);
+        lseq.delete(2);
+
+        expect(lseq.query().join('')).toBe("hi*!");
+    });
+
+    test("Test Merge", () => {
+        let lseq1 = new LSeq("node1");
+        let lseq2 = new LSeq("node2");
+
+        lseq1.insert(0, 'hi');
+        lseq1.insert(1, '*');
+        lseq1.insert(2, '!');
+
+        expect(lseq1.query().join('')).toBe("hi*!");
+
+        lseq2.merge(lseq1);
+
+        expect(lseq2.query().join('')).toBe("hi*!");
+
+        lseq1.insert(2, 'mom');
+        lseq2.insert(2 ,'dad');
+
+        expect(lseq1.query().join('')).toBe("hi*mom!");
+        expect(lseq2.query().join('')).toBe("hi*dad!");
+
+        lseq1.merge(lseq2);
+        lseq2.merge(lseq1);
+
+        expect(lseq1.query().join('')).toBe("hi*dadmom!");
+        expect(lseq2.query().join('')).toBe("hi*dadmom!");
+    });
 });
