dd3d2ee7a93165599a83d7fad75467aaa72a64ce
h3lio5 <akashsm@ce.iitr.ac.in>
2024-08-08 18:26:05 +0530

    feat: implement a (basic) linearize method on hashgraph

--- packages/object/src/hashgraph.ts
+++ packages/object/src/hashgraph.ts
@@ -1,6 +1,6 @@
 export interface IHashGraphDAG {
   getFrontier(): Array<string>;
-  getDependencies(id: string): Array<string>;
+  getDependencies(hash: string): Array<string>;
   addNode(op: string, dependecies: Array<string>);
   getOriginalState(): any;
   getOriginSet(): Array<string>;
@@ -8,46 +8,51 @@ export interface IHashGraphDAG {
 
 }
 
-class HashgraphNode<T> {
+class HashgraphNode<O, T> {
   readonly hash: Hash;
-  readonly operation: T;
+  readonly operation: O;
+  readonly tick: T;
   readonly dependencies: Set<Hash>;
 
-  constructor(hash: Hash, operation: T, operation: Set<Hash> = new Set()) {
+  constructor(hash: Hash, operation: O, tick: T, dependencies: Set<Hash> = new Set()) {
     this.hash = hash;
     this.operation = operation;
+    this.tick = tick;
     this.dependencies = dependencies;
+  }
+
+  getDependeciesCount(): integer {
+    this.depedencies.size
+  }
+
+  getDependencies(): Hash[] {
+    [].concat(...this.dependencies)
   }
 }
 
-class HashgraphDAG<T> {
+class HashgraphDAG<O,T> {
   private nodes: Map<Hash, HashgraphNode<T>> = new Map();
   private frontier: Set<Hash> = new Set();
   private originSet: Set<Hash> = new Set();
   private precedenceRules: Map<string, boolean> = new Map();
+  private tickStore: Map<O, Hash> = new Map();
 
-  constructor(precedenceRules: any) {
-    // precedence rules for conflict-semantics
-    // precedenceRules is of the form [(opA, opB, true), (opA, opC, false), ..]
-    // Create a mapping for each rule as (opA, opB, true) -> precedenceRules[opA || opB] = true, precedenceRules[opB || opA] = false.
-    for (const entry of precedenceRules) {
-      const (opA, opB, isPrecede) = entry;
-      this.precedenceRules[opA.concat(opB)] = isPrecede;
-      this.precedenceRules[opB.concat(opA)] = !isPrecede;
-    }
+  constructor() {
+      // could set the precedenceRules here
   }
 
-  addNode(hash: Hash, operation: T, dependencies: Hash[] = []): void {
+  addNode(hash: Hash, operation: O, tick: T, dependencies: Hash[] = []): void {
     if (this.nodes.has(hash)) {
       throw new Error(`Node with hash ${hash} already exists`);
     }
 
     const dependenciesSet = new Set(dependencies);
-    const node = new HashgraphNode(hash, operation, dependenciesSet);
+    const node = new HashgraphNode(hash, operation, tick, dependenciesSet);
 
     this.nodes.set(hash, node);
     this.frontier.add(hash);
-
+    this.tickStore[tick].push(hash);
+    
     // if the node has no dependencies place it in the originSet
     if (dependencies.len() === 0) {
       this.originSet.set(hash, node);
@@ -67,24 +72,77 @@ class HashgraphDAG<T> {
     return Array.from(this.frontier);
   }
 
-  getSerializedOperations(): Hash[] {
-    const visited = new Set<Hash>();
+  getOps(tick: T): O[] {
+    // fetch all the ops at the tick `tick`
+    const nodeHashes = this.tickStore.get(tick);
+    const ops = nodeHashes.map(hash => this.nodes.get(hash).operation);
+    // apply conflict semantics
+    const result = this.applyConflictSemantics(ops)
+
+    return result
+  }
+
+  applyConflictSemantics(ops: O[]) {
+    
+  }
+
+
+  getLinearOps(): Hash[] {
     const result: Hash[] = [];
 
-    const visit = (hash: Hash) => {
-      if (!visited.has(hash)) {
-        visited.add(hash);
-        const node = this.nodes.get(hash)!;
-        for (const parentHash of node.parents) {
-          visit(parentHash);
-        }
-        result.unshift(hash);
-      }
-    };
+    const outDegrees: Map<Hash, number> = new Map(); 
+    const inverseDependencies : Map<Hash, Hash[]> = new Map();
 
-    for (const hash of this.nodes.keys()) {
-      visit(hash);
+    for (const [nodeHash, node] of this.nodes.entries()) {
+      // Compute the out degrees of each node
+      outDegrees[nodeHash] = node.getDependenciesCount();
+    
+      // compute the mapping that stores node -> [dependencies]
+      const dependencies = node.getDependencies();
+      for (const dependency of dependencies) {
+        inverseDependencies[dependency].push(nodeHash); 
+      }
     }
+
+    // Initialize the queue
+    const queue: Hash[] = [];
+    outDegrees.forEach((key, value) => {
+      if (value === 0) {
+        queue.push(key);
+      }
+    }
+
+    while (queue.length > 0) {
+      const nodeHash = queue.shift();
+      const nodeOp = this.nodes.get(nodeHash).operation;
+      result.push(nodeOp);
+      
+      // Decrement the outDegree of each dependency by 1
+      const dependencyHashes = inverseDependencies[nodeHash];
+      const zeroOutDegreeDependencyOps: Hash[] = [];
+
+      for (const dependencyHash of dependencyHashes) {
+        const currentDegree = outDegrees[dependencyHash]; 
+        outDegrees.set(dependencyHash, currentDegree - 1);
+        
+        if currentDegree - 1 ===0 {
+          const op = this.nodes.get(dependencyHash).operation;
+          zeroOutDegreeDependencyOps.set(op, dependencyHash);
+        }
+      }
+      
+      const dependencyOps = Array.from(zeroOutDegreeDependencyOps.keys());
+      // apply conflict semantics to the dependencyOps
+      const ops = this.applyConflictSemantics(dependencyOps);
+      for (const op of ops) {
+        queue.push(zeroOutDegreeDependencyOps.get(op));
+      }
+    }
+
+    // Check if topological sort is possible (i.e., the graph is a DAG)
+    // if (topologicalOrder.length !== nodes.size) {
+    // throw new Error("Graph is not a DAG (contains a cycle)");
+    //}
 
     return result;
   }
