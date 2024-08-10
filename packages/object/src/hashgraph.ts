609f70959b2434dcab11d90e2e0ab9de4d3e7dc2
h3lio5 <akashsm@ce.iitr.ac.in>
2024-08-10 18:52:51 +0530

    fix: fix compile errors

--- packages/object/src/hashgraph.ts
+++ packages/object/src/hashgraph.ts
@@ -1,65 +1,64 @@
-export interface IHashGraphDAG<O,T> {
-  addNode(hash: Hash, operation: O, tick: T, dependecies: Hash[]);
+export interface IHashGraphDAG<T> {
+  addNode(hash: Hash, operation: T, tick: number, dependecies: Hash[]): void;
   getFrontier(): Hash[];
   getDependencies(hash: Hash): Hash[];
   getOriginalState(): any;
-  getOriginSet(): Array<Hash>;
-  getOps(tick: T): O[];
-  getLinearOps(): O[];
-  getNode(hash: Hash): HashgraphNode<O, T> | undefined;
-  getAllNodes(): HashgraphNode<O, T>[];
+  getOriginSet(): Hash[];
+  getOps(tick: number): T[];
+  getLinearOps(): T[];
+  getNode(hash: Hash): HashgraphNode<T> | undefined;
+  getAllNodes(): HashgraphNode<T>[];
 }
 
 type Hash  = string;
 
-class HashgraphNode<O, T> {
+class HashgraphNode<T> {
   readonly hash: Hash;
-  readonly operation: O;
-  readonly tick: T;
+  readonly operation: T;
   readonly dependencies: Set<Hash>;
 
-  constructor(hash: Hash, operation: O, tick: T, dependencies: Set<Hash> = new Set()) {
+  constructor(hash: Hash, operation: T, dependencies: Set<Hash> = new Set()) {
     this.hash = hash;
     this.operation = operation;
-    this.tick = tick;
     this.dependencies = dependencies;
   }
 
-  getDependeciesCount(): integer {
-    this.depedencies.size
+  getDependeciesCount(): number {
+    return this.dependencies.size;
   }
 
   getDependencies(): Hash[] {
-    [].concat(...this.dependencies)
+    return [...this.dependencies];
   }
 }
 
-class HashgraphDAG<O,T> {
+class HashgraphDAG<T> {
   private nodes: Map<Hash, HashgraphNode<T>> = new Map();
   private frontier: Set<Hash> = new Set();
   private originSet: Set<Hash> = new Set();
   private precedenceRules: Map<string, boolean> = new Map();
-  private tickStore: Map<O, Hash> = new Map();
+  private tickStore: Map<number, Hash[]> = new Map();
+  private latestTick: number = 0;
 
   constructor() {
       // could set the precedenceRules here
   }
 
-  addNode(hash: Hash, operation: O, tick: T, dependencies: Hash[] = []): void {
+  addNode(hash: Hash, operation: T, tick: number, dependencies: Hash[] = []): void {
     if (this.nodes.has(hash)) {
-      throw new Error(`Node with hash ${hash} already exists`);
+      console.log(`Node with hash ${hash} already exists`);
     }
 
     const dependenciesSet = new Set(dependencies);
-    const node = new HashgraphNode(hash, operation, tick, dependenciesSet);
+    const node = new HashgraphNode(hash, operation, dependenciesSet);
 
     this.nodes.set(hash, node);
     this.frontier.add(hash);
-    this.tickStore[tick].push(hash);
+    this.tickStore.set(tick, [...(this.tickStore.get(tick) || []), hash]);
     
     // if the node has no dependencies place it in the originSet
-    if (dependencies.len() === 0) {
-      this.originSet.set(hash, node);
+    if (dependencies.length === 0) {
+      this.originSet.add(hash);
     }
 
     for (const dependencyHash of dependencies) {
@@ -67,7 +66,7 @@ class HashgraphDAG<O,T> {
       if (dependencyNode) {
         this.frontier.delete(dependencyHash);
       } else {
-        throw new Error(`Dependency node ${dependencyHash} does not exist`);
+        console.log(`Dependency node ${dependencyHash} does not exist`);
       }
     }
   }
@@ -81,72 +80,70 @@ class HashgraphDAG<O,T> {
   }
 
   getDependences(hash: Hash): Hash[] {
-    return this.nodes.get(hash).getDependencies();
+    return this.nodes.get(hash)?.getDependencies() || [];
   }
 
-  getOps(tick: T): O[] {
+  getOps(tick: number): T[] {
     // fetch all the ops at the tick `tick`
-    const nodeHashes = this.tickStore.get(tick);
-    const ops = nodeHashes.map(hash => this.nodes.get(hash).operation);
+    const nodeHashes = this.tickStore.get(tick) ?? [];
+    const ops = nodeHashes.map(hash => this.nodes.get(hash)?.operation!);
     // apply conflict semantics
     const result = this.applyConflictSemantics(ops)
 
     return result
   }
 
-  applyConflictSemantics(ops: O[]) {
-    
+  applyConflictSemantics(ops: T[]): T[] {
+    return ops;
   }
 
-  getLinearOps(): O[] {
-    const result: Hash[] = [];
+  getLinearOps(): T[] {
+    const result: T[] = [];
 
     const outDegrees: Map<Hash, number> = new Map(); 
     const inverseDependencies : Map<Hash, Hash[]> = new Map();
 
     for (const [nodeHash, node] of this.nodes.entries()) {
       // Compute the out degrees of each node
-      outDegrees[nodeHash] = node.getDependenciesCount();
+      outDegrees.set(nodeHash, node.getDependeciesCount());
     
       // compute the mapping that stores node -> [dependencies]
       const dependencies = node.getDependencies();
       for (const dependency of dependencies) {
-        inverseDependencies[dependency].push(nodeHash); 
+        inverseDependencies.set(dependency, [...inverseDependencies.get(dependency) || [], nodeHash]);
       }
     }
 
     // Initialize the queue
     const queue: Hash[] = [];
-    outDegrees.forEach((key, value) => {
-      if (value === 0) {
-        queue.push(key);
-      }
-    }
+    outDegrees.forEach((value, key) => value === 0 && queue.push(key));
 
     while (queue.length > 0) {
-      const nodeHash = queue.shift();
-      const nodeOp = this.nodes.get(nodeHash).operation;
+      const nodeHash = queue.shift()!;
+      const nodeOp = this.nodes.get(nodeHash)?.operation!;
       result.push(nodeOp);
       
       // Decrement the outDegree of each dependency by 1
-      const dependencyHashes = inverseDependencies[nodeHash];
-      const zeroOutDegreeDependencyOps: Map<O, Hash> = new Map();
+      const dependentHashes = inverseDependencies.get(nodeHash) || [];
+      // zeroOutDegreeDependencyOps is an inverse mapping (ops -> nodeHash) which 
+      // is needed to get the nodeHash after applyConflictSemantics applied on the ops.
+      const zeroOutDegreeDependentOps: Map<T, Hash> = new Map();
 
-      for (const dependencyHash of dependencyHashes) {
-        const currentDegree = outDegrees[dependencyHash]; 
-        outDegrees.set(dependencyHash, currentDegree - 1);
+      for (const dependentHash of dependentHashes) {
+        const currentDegree = outDegrees.get(dependentHash)!;
+        outDegrees.set(dependentHash, currentDegree - 1);
         
-        if currentDegree - 1 === 0 {
-          const op = this.nodes.get(dependencyHash).operation;
-          zeroOutDegreeDependencyOps.set(op, dependencyHash);
+        if (currentDegree - 1 === 0) {
+          const op = this.nodes.get(dependentHash)?.operation!;
+          zeroOutDegreeDependentOps.set(op, dependentHash);
         }
       }
       
-      const dependencyOps = Array.from(zeroOutDegreeDependencyOps.keys());
+      const dependentOps = Array.from(zeroOutDegreeDependentOps.keys());
       // apply conflict semantics to the dependencyOps
-      const ops = this.applyConflictSemantics(dependencyOps);
+      const ops = this.applyConflictSemantics(dependentOps);
       for (const op of ops) {
-        queue.push(zeroOutDegreeDependencyOps.get(op));
+        queue.push(zeroOutDegreeDependentOps.get(op)!);
       }
     }
 
