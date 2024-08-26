90a4f3d98565105542534ffa2d80e9fc730029a8
João Pereira <77340776+joaopereira12@users.noreply.github.com>
2024-08-26 14:53:22 +0900

    feat: OOR-Set CRDT & Tests (#72)
    Co-authored-by: droak <me+git@droak.sh>

--- packages/crdt/src/index.ts
+++ packages/crdt/src/index.ts
@@ -4,6 +4,7 @@ export * from "./crdts/GSet/index.js";
 export * from "./crdts/IPSet/index.js";
 export * from "./crdts/LWWElementSet/index.js";
 export * from "./crdts/LWWRegister/index.js";
+export * from "./crdts/OORSet/index.js";
 export * from "./crdts/PNCounter/index.js";
 export * from "./crdts/RGA/index.js";
 
