e4293bf4a7a0a736f968478de956edc3935b7aa2
winprn <minhladz001@gmail.com>
2024-10-29 12:35:09 +0700

    chore(biome): update import order

--- packages/network/src/node.ts
+++ packages/network/src/node.ts
@@ -27,13 +27,13 @@ import { webSockets } from "@libp2p/websockets";
 import * as filters from "@libp2p/websockets/filters";
 import { webTransport } from "@libp2p/webtransport";
 import { multiaddr } from "@multiformats/multiaddr";
+import type { LoggerOptions } from "@topology-foundation/logger";
 import { type Libp2p, createLibp2p } from "libp2p";
+import loglevel from "loglevel";
+import prefix from "loglevel-plugin-prefix";
 import { fromString as uint8ArrayFromString } from "uint8arrays/from-string";
 import { Message } from "./proto/messages_pb.js";
 import { uint8ArrayToStream } from "./stream.js";
-import type { LoggerOptions } from "@topology-foundation/logger";
-import loglevel from "loglevel";
-import prefix from "loglevel-plugin-prefix";
 
 export * from "./stream.js";
 
