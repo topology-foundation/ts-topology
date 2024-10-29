b29c2658d3218a0aa9da4aa1c0399a5a68b5ebef
trungnotchung <trung09072004@gmail.com>
2024-10-29 15:29:23 +0700

    fix: remove joi dependency

--- packages/node/src/config.ts
+++ packages/node/src/config.ts
@@ -1,32 +1,31 @@
+import fs from "node:fs";
 import * as dotenv from "dotenv";
-import Joi from "joi";
+import { program } from "./cli/index.js";
 
-dotenv.config();
+program.parse(process.argv);
+const opts = program.opts();
 
-const envVarsSchema = Joi.object({
-	ADDRESSES: Joi.string().optional(),
-	BOOTSTRAP: Joi.boolean().default(false),
-	BOOTSTRAP_PEERS: Joi.string().optional(),
-	BROWSER_METRICS: Joi.boolean().default(false),
-	PRIVATE_KEY_SEED: Joi.string().optional(),
-});
+const initializeEnv = () => {
+	if (opts.config) {
+		const configFile = JSON.parse(fs.readFileSync(opts.config, "utf8"));
+		return JSON.parse(fs.readFileSync(opts.config, "utf8"));
+	}
 
-const { value: envVars, error } = envVarsSchema.validate(process.env, {
-	allowUnknown: true,
-});
-
-if (error) {
-	throw new Error(`Config validation error: ${error.message}`);
-}
-
-export const env = {
-	addresses: envVars.ADDRESSES ? envVars.ADDRESSES.split(",") : undefined,
-	bootstrap: envVars.BOOTSTRAP,
-	bootstrap_peers: envVars.BOOTSTRAP_PEERS
-		? envVars.BOOTSTRAP_PEERS.split(",")
-		: undefined,
-	browser_metrics: envVars.BROWSER_METRICS,
-	private_key_seed: envVars.PRIVATE_KEY_SEED,
+	const result = dotenv.config();
+	if (result.error) {
+		throw result.error;
+	}
+	return {
+		network_config: {
+			addresses: process.env.ADDRESSES ? process.env.ADDRESSES.split(",") : [],
+			bootstrap: process.env.BOOTSTRAP === "true",
+			bootstrap_peers: process.env.BOOTSTRAP_PEERS
+				? process.env.BOOTSTRAP_PEERS.split(",")
+				: [],
+			browser_metrics: process.env.BROWSER_METRICS === "true",
+			private_key_seed: process.env.PRIVATE_KEY_SEED,
+		},
+	};
 };
 
-export default env;
+export const env = initializeEnv();
--- packages/node/src/run.ts
+++ packages/node/src/run.ts
@@ -1,19 +1,9 @@
-import fs from "node:fs";
-import { program } from "./cli/index.js";
 import { env } from "./config.js";
 import { TopologyNode, type TopologyNodeConfig } from "./index.js";
 import { init as rpc_init } from "./rpc/index.js";
 
 const run = async () => {
-	program.parse(process.argv);
-	const opts = program.opts();
-	let config: TopologyNodeConfig | undefined;
-	if (opts.config) {
-		config = JSON.parse(fs.readFileSync(opts.config, "utf8"));
-	} else {
-		config = env as TopologyNodeConfig;
-	}
-
+	const config: TopologyNodeConfig | undefined = env as TopologyNodeConfig;
 	const node = new TopologyNode(config);
 	await node.start();
 	rpc_init(node);
