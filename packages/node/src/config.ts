dc369d1c9033270412aebe953adbec2171646171
trungnotchung <trung09072004@gmail.com>
2024-10-30 10:40:01 +0700

    fix: rename function and remove hardcoded defaults

--- packages/node/src/config.ts
+++ packages/node/src/config.ts
@@ -1,31 +1,34 @@
 import fs from "node:fs";
 import * as dotenv from "dotenv";
-import { program } from "./cli/index.js";
+import type { TopologyNodeConfig } from "./index.js";
 
-program.parse(process.argv);
-const opts = program.opts();
+export let config: TopologyNodeConfig = {};
 
-const initializeEnv = () => {
-	if (opts.config) {
-		const configFile = JSON.parse(fs.readFileSync(opts.config, "utf8"));
-		return JSON.parse(fs.readFileSync(opts.config, "utf8"));
+export function loadConfig(configPath?: string | undefined) {
+	if (configPath) {
+		config = JSON.parse(fs.readFileSync(configPath, "utf8"));
+		return;
 	}
 
 	const result = dotenv.config();
-	if (result.error) {
-		throw result.error;
-	}
-	return {
-		network_config: {
-			addresses: process.env.ADDRESSES ? process.env.ADDRESSES.split(",") : [],
-			bootstrap: process.env.BOOTSTRAP === "true",
+	if (!result.error) {
+		config.network_config = {
+			addresses: process.env.ADDRESSES
+				? process.env.ADDRESSES.split(",")
+				: undefined,
+			bootstrap: process.env.BOOTSTRAP
+				? process.env.BOOTSTRAP === "true"
+				: undefined,
 			bootstrap_peers: process.env.BOOTSTRAP_PEERS
 				? process.env.BOOTSTRAP_PEERS.split(",")
-				: [],
-			browser_metrics: process.env.BROWSER_METRICS === "true",
-			private_key_seed: process.env.PRIVATE_KEY_SEED,
-		},
-	};
-};
-
-export const env = initializeEnv();
+				: undefined,
+			browser_metrics: process.env.BROWSER_METRICS
+				? process.env.BROWSER_METRICS === "true"
+				: undefined,
+			private_key_seed: process.env.PRIVATE_KEY_SEED
+				? process.env.PRIVATE_KEY_SEED
+				: undefined,
+		};
+		return;
+	}
+}
--- packages/node/src/run.ts
+++ packages/node/src/run.ts
@@ -1,9 +1,13 @@
-import { env } from "./config.js";
-import { TopologyNode, type TopologyNodeConfig } from "./index.js";
+import { program } from "./cli/index.js";
+import { config, loadConfig } from "./config.js";
+import { TopologyNode } from "./index.js";
 import { init as rpc_init } from "./rpc/index.js";
 
 const run = async () => {
-	const config: TopologyNodeConfig | undefined = env as TopologyNodeConfig;
+	program.parse(process.argv);
+	const opts = program.opts();
+	loadConfig(opts.config);
+
 	const node = new TopologyNode(config);
 	await node.start();
 	rpc_init(node);
