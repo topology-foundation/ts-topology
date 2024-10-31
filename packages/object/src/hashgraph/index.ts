ab4ca26257533617823c6140be4142bc6d97f55f
hoangquocvietuet <hoangquocvietuet@gmail.com>
2024-10-31 14:06:15 +0700

    chore: remove typo

--- packages/object/src/hashgraph/index.ts
+++ packages/object/src/hashgraph/index.ts
@@ -127,7 +127,7 @@ export class HashGraph {
 			return hash; // Vertex already exists
 		}
 
-		// Temporary fix: don't add the vertex if the dependencies are not present in the local HG.dw
+		// Temporary fix: don't add the vertex if the dependencies are not present in the local HG.
 		if (
 			!deps.every((dep) => this.forwardEdges.has(dep) || this.vertices.has(dep))
 		) {
