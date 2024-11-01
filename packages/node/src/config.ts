104b8297f4f5cd35a40ac478acda359942c186da
droak <me+git@droak.sh>
2024-11-01 12:51:08 +0900

    add missing constructor for config

--- packages/node/src/config.ts
+++ packages/node/src/config.ts
@@ -14,6 +14,7 @@ export function loadConfig(
 
 	const result = dotenv.config();
 	if (!result.error) {
+		config = {};
 		config.network_config = {
 			addresses: process.env.ADDRESSES
 				? process.env.ADDRESSES.split(",")
