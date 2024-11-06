eb2750e253b6c1b272206ae58e506754f4364bad
elielnfinic <46634774+elielnfinic@users.noreply.github.com>
2024-11-06 02:53:53 +0200

    Removing mapper

--- packages/network/src/node.ts
+++ packages/network/src/node.ts
@@ -73,8 +73,8 @@ export class TopologyNetworkNode {
 			? this._config.bootstrap_peers
 			: [
 					// "/dns4/relay.droak.sh/tcp/443/wss/p2p/Qma3GsJmB47xYuyahPZPSadh1avvxfyYQwk8R3UnFrQ6aP",
-					"/ip4/127.0.0.1/tcp/50000/ws/p2p/12D3KooWC6sm9iwmYbeQJCJipKTRghmABNz1wnpJANvSMabvecwJ",
-					// "/dns4/topology-1.nfinic.com/tcp/4430/wss/p2p/12D3KooWC6sm9iwmYbeQJCJipKTRghmABNz1wnpJANvSMabvecwJ",
+					// "/ip4/127.0.0.1/tcp/50000/ws/p2p/12D3KooWC6sm9iwmYbeQJCJipKTRghmABNz1wnpJANvSMabvecwJ",
+					"/dns4/topology-1.nfinic.com/tcp/4430/wss/p2p/12D3KooWC6sm9iwmYbeQJCJipKTRghmABNz1wnpJANvSMabvecwJ",
 				];
 
 		const _peerDiscovery = _bootstrapNodesList.length
@@ -94,7 +94,7 @@ export class TopologyNetworkNode {
 				protocol: "/topology/dht/1.0.0",
 				kBucketSize: this._config?.bootstrap ? 40 : 20,
 				clientMode: false,
-				peerInfoMapper: removePrivateAddressesMapper,
+				// peerInfoMapper: removePrivateAddressesMapper,
 				querySelfInterval: 20000,
 				initialQuerySelfInterval: 10000,
 				allowQueryWithZeroPeers: false,
