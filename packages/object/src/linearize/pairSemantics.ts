d55042dd7c07faf8a51614c4c667656b6e572845
trungnotchung <trung09072004@gmail.com>
2024-11-27 19:05:27 +0700

    update linearization logic pair semantics

--- packages/object/src/linearize/pairSemantics.ts
+++ packages/object/src/linearize/pairSemantics.ts
@@ -5,70 +5,7 @@ import {
 	type Operation,
 } from "../hashgraph/index.js";
 
-export function fullLinearization(hashGraph: HashGraph): Operation[] {
-	const order: Hash[] = hashGraph.topologicalSort(true);
-	const dropped = new Array(order.length).fill(false);
-	const result = [];
-	let i = 0;
-
-	while (i < order.length) {
-		if (dropped[i]) {
-			i++;
-			continue;
-		}
-		const anchor = order[i];
-		let j = i + 1;
-
-		while (j < order.length) {
-			if (dropped[j]) {
-				j++;
-				continue;
-			}
-			const moving = order[j];
-
-			if (!hashGraph.areCausallyRelatedUsingBitsets(anchor, moving)) {
-				const v1 = hashGraph.vertices.get(anchor);
-				const v2 = hashGraph.vertices.get(moving);
-				let action: ActionType;
-				if (!v1 || !v2) {
-					action = ActionType.Nop;
-				} else {
-					action = hashGraph.resolveConflicts([v1, v2]).action;
-				}
-
-				switch (action) {
-					case ActionType.DropLeft:
-						dropped[i] = true;
-						j = order.length;
-						break;
-					case ActionType.DropRight:
-						dropped[j] = true;
-						j++;
-						break;
-					case ActionType.Swap:
-						[order[i], order[j]] = [order[j], order[i]];
-						j = i + 1;
-						break;
-					case ActionType.Nop:
-						j++;
-						break;
-				}
-			} else {
-				j++;
-			}
-		}
-
-		if (!dropped[i]) {
-			const op = hashGraph.vertices.get(order[i])?.operation;
-			if (op && op.value !== null) result.push(op);
-		}
-		i++;
-	}
-
-	return result;
-}
-
-export function partialLinearization(
+export function linearizePairSemantics(
 	hashGraph: HashGraph,
 	origin: Hash,
 	subgraph: Set<string>,
@@ -87,41 +24,40 @@ export function partialLinearization(
 		let j = i + 1;
 
 		while (j < order.length) {
-			if (dropped[j]) {
+			if (
+				hashGraph.areCausallyRelatedUsingBitsets(anchor, order[j]) ||
+				dropped[j]
+			) {
 				j++;
 				continue;
 			}
 			const moving = order[j];
 
-			if (!hashGraph.areCausallyRelatedUsingBitsets(anchor, moving)) {
-				const v1 = hashGraph.vertices.get(anchor);
-				const v2 = hashGraph.vertices.get(moving);
-				let action: ActionType;
-				if (!v1 || !v2) {
-					action = ActionType.Nop;
-				} else {
-					action = hashGraph.resolveConflicts([v1, v2]).action;
-				}
-
-				switch (action) {
-					case ActionType.DropLeft:
-						dropped[i] = true;
-						j = order.length;
-						break;
-					case ActionType.DropRight:
-						dropped[j] = true;
-						j++;
-						break;
-					case ActionType.Swap:
-						[order[i], order[j]] = [order[j], order[i]];
-						j = i + 1;
-						break;
-					case ActionType.Nop:
-						j++;
-						break;
-				}
+			const v1 = hashGraph.vertices.get(anchor);
+			const v2 = hashGraph.vertices.get(moving);
+			let action: ActionType;
+			if (!v1 || !v2) {
+				action = ActionType.Nop;
 			} else {
-				j++;
+				action = hashGraph.resolveConflicts([v1, v2]).action;
+			}
+
+			switch (action) {
+				case ActionType.DropLeft:
+					dropped[i] = true;
+					j = order.length;
+					break;
+				case ActionType.DropRight:
+					dropped[j] = true;
+					j++;
+					break;
+				case ActionType.Swap:
+					[order[i], order[j]] = [order[j], order[i]];
+					j = i + 1;
+					break;
+				case ActionType.Nop:
+					j++;
+					break;
 			}
 		}
 
@@ -133,15 +69,4 @@ export function partialLinearization(
 	}
 
 	return result;
-}
-
-export function linearizePairSemantics(
-	hashGraph: HashGraph,
-	origin: Hash,
-	subgraph: Set<string>,
-): Operation[] {
-	if (subgraph.size === hashGraph.vertices.size) {
-		return fullLinearization(hashGraph);
-	}
-	return partialLinearization(hashGraph, origin, subgraph);
 }
